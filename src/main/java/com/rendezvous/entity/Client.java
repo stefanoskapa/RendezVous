905879b9727b55dfa983fba6bfca688bf7e11cc5
Stefanos Koutsouflakis <koutsouflakis.stefanos@gmail.com>
2021-03-11 13:19:21 +0200

    remove dto. Basic Client-registration. Tables Client, User and User_roles are being updated

--- src/main/java/com/rendezvous/entity/Client.java
+++ src/main/java/com/rendezvous/entity/Client.java
@@ -20,6 +20,7 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -62,6 +63,7 @@ public class Client implements Serializable {
     
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     @ManyToOne(optional = false)
+    @Valid
     private User userId;
     
 //    @OneToMany(cascade = CascadeType.ALL, mappedBy = "clientId")
