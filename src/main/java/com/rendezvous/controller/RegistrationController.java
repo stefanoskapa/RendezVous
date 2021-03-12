e015c30d4e37e6fae65b136d1805e88eebb7da07
Stefanos Koutsouflakis <koutsouflakis.stefanos@gmail.com>
2021-03-12 21:27:52 +0200

    moved some code from controllers to services

--- src/main/java/com/rendezvous/controller/RegistrationController.java
+++ src/main/java/com/rendezvous/controller/RegistrationController.java
@@ -1,55 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.rendezvous.controller;
 
 import com.rendezvous.entity.Client;
 import com.rendezvous.entity.Company;
-import com.rendezvous.entity.Role;
-import com.rendezvous.entity.User;
-import com.rendezvous.repository.ClientRepository;
-import com.rendezvous.repository.CompanyRepository;
-import com.rendezvous.repository.RoleRepository;
 import com.rendezvous.repository.UserRepository;
-import java.util.ArrayList;
-import java.util.List;
+import com.rendezvous.service.ClientService;
+import com.rendezvous.service.CompanyService;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class RegistrationController {
 
-    @Autowired
-    private RoleRepository roleRepository;
+    
     @Autowired
     private UserRepository userRepository;
     @Autowired
-    private ClientRepository clientRepository;
+    private ClientService clientService;
     @Autowired
-    private CompanyRepository companyRepository;
-    @Autowired
-    private JdbcUserDetailsManager jdbcUserDetailsManager;
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private CompanyService companyService;
+    
 
     @GetMapping("/client-register")
     public String showClientRegistration(@ModelAttribute("newClient") Client newClient, Model model) {
-
         return "client/register_client";
     }
 
-    @PostMapping("/client-register") //todo check if user email already exists
+    @PostMapping("/client-register")
     public String clientRegistration(@Valid @ModelAttribute("newClient") Client newClient, BindingResult bindingResult, Model model) {
 
         if (bindingResult.hasErrors()) {
@@ -60,17 +42,8 @@ public class RegistrationController {
             model.addAttribute("newClient", new Client());
             return "client/register_client";
         }
-        List<Role> userRole = new ArrayList<>();
-        List<Role> roles = roleRepository.findAll();
-        for (Role a : roles) {
-            if (a.getRole().equals("ROLE_CLIENT")) {
-                userRole.add(a);
-            }
-        }
-        newClient.getUser().setRoleList(userRole);
-        String encodedPassword = bCryptPasswordEncoder.encode(newClient.getUser().getPassword());
-        newClient.getUser().setPassword(encodedPassword);
-        clientRepository.save(newClient);
+        clientService.saveClient(newClient);
+        
         return "redirect:/";
     }
 
@@ -90,19 +63,8 @@ public class RegistrationController {
             model.addAttribute("newCompany", new Company());
             return "company/register_company";
         }
-
-        List<Role> userRole = new ArrayList<>();
-        List<Role> roles = roleRepository.findAll();
-        for (Role a : roles) {
-            if (a.getRole().equals("ROLE_COMPANY")) {
-                userRole.add(a);
-            }
-        }
-        newCompany.getUser().setRoleList(userRole);
-        String encodedPassword = bCryptPasswordEncoder.encode(newCompany.getUser().getPassword());
-        newCompany.getUser().setPassword(encodedPassword);
-        companyRepository.save(newCompany);
-
+        
+        companyService.saveCompany(newCompany);
         return "redirect:/login";
 
     }
--- src/main/java/com/rendezvous/service/ClientService.java
+++ src/main/java/com/rendezvous/service/ClientService.java
@@ -6,10 +6,16 @@
 package com.rendezvous.service;
 
 import com.rendezvous.entity.Client;
+import com.rendezvous.entity.Role;
 import com.rendezvous.repository.ClientRepository;
+import com.rendezvous.repository.RoleRepository;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,10 +23,28 @@ public class ClientService {
     
     @Autowired
     ClientRepository clientRepository;
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private JdbcUserDetailsManager jdbcUserDetailsManager;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     
     public Client findClientByEmail(String email){
         Optional<Client> client = clientRepository.findClientByUserEmail(email);
         client.orElseThrow(()-> new UsernameNotFoundException("User " + email + " not found!"));
         return client.get();
+    }
+    
+    public void saveClient(Client client) {
+    List<Role> roles = roleRepository.findAll();
+        for (Role a : roles) {
+            if (a.getRole().equals("ROLE_CLIENT")) {
+                client.getUser().setRoleList(Arrays.asList(a));
+            }
+        }
+        String encodedPassword = bCryptPasswordEncoder.encode(client.getUser().getPassword());
+        client.getUser().setPassword(encodedPassword);
+        clientRepository.save(client);
     }
 }
