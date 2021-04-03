8f96b008b81e7f8cf6c584ab5e0307a3c9c3cef1
Stefanos Koutsouflakis <koutsouflakis.stefanos@gmail.com>
2021-04-03 23:56:22 +0300

    Chat controller clean-up

--- src/main/java/com/rendezvous/controller/ChatController.java
+++ src/main/java/com/rendezvous/controller/ChatController.java
@@ -17,8 +17,6 @@ import com.rendezvous.repository.CompanyRepository;
 import com.rendezvous.repository.ConversationRepository;
 import com.rendezvous.repository.UserRepository;
 import com.rendezvous.service.MessagesService;
-import com.rendezvous.service.NotificationDispatcher;
-import com.rendezvous.util.Conversion;
 import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.LinkedList;
@@ -30,7 +28,6 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,8 +42,6 @@ public class ChatController {
     @Autowired
     private MessagesService messagesService;
     @Autowired
-    private NotificationDispatcher dispatcher;
-    @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
     @Autowired
     private ClientRepository clientRepository;
@@ -59,88 +54,49 @@ public class ChatController {
             Principal user,
             @Header("simpSessionId") String sessionId) throws Exception {
         msg.setFrom(user.getName());
-
         simpMessagingTemplate.convertAndSendToUser(
                 msg.getTo(), "/secured/user/queue/specific-user", msg);
-        User tempUser = userRepository.findByEmail(user.getName()).get();
-        User tempUser2 = userRepository.findByEmail(msg.getTo()).get();
+        User tempUser = userRepository.findByEmail(user.getName()).get(); // sender
+        User tempUser2 = userRepository.findByEmail(msg.getTo()).get(); // recipient
         Conversation conv = conversationRepository.findConversation(tempUser.getId(), tempUser2.getId());
         Messages tempMessage = new Messages(tempUser.getId(), conv.getId(),msg.getText(),LocalDateTime.now());
         messagesService.save(tempMessage, conv);
     }
 
-    @MessageMapping("/start")
-    public void start(StompHeaderAccessor stompHeaderAccessor) {
-        dispatcher.add(stompHeaderAccessor.getUser().getName(), stompHeaderAccessor.getSessionId());
-    }
-
-    @MessageMapping("/client/history")
-    public void addClientMessageToHistory(Messages message, Principal principal) {
-        if (userRepository.findById(message.getUserId()).getEmail().equals(principal.getName())) {
-            Conversation conv = conversationRepository.findById(message.getConversationId()).get();
-            message.setTimestamp(Conversion.adjustTime(message.getTimestamp()));
-            messagesService.save(message, conv);
-        }
-    }
-
-    @MessageMapping("/company/history")
-    public void addCompanyMessageToHistory(Messages message, Principal principal) {
-        if (userRepository.findById(message.getUserId()).getEmail().equals(principal.getName())) {
-            Conversation conv = conversationRepository.findById(message.getConversationId()).get();
-            message.setTimestamp(Conversion.adjustTime(message.getTimestamp()));
-            messagesService.save(message, conv);
-        }
-    }
-
-    @GetMapping("/conv") //has to return a list of full UserProps
+    @GetMapping("/conv") //returns list of conversation-partners
     public ResponseEntity<List<UserProps>> fetchConvPartners(Principal principal) {
         User tempUser = userRepository.findByEmail(principal.getName()).get();
         List<Conversation> conv = conversationRepository.findByUserId(tempUser.getId());
         List<UserProps> convPartners = new LinkedList<>();
         for (Conversation a : conv) {
-            boolean is_comp = tempUser.getRoleList().get(0).getRole().equals("ROLE_COMPANY");
-            if (is_comp) {
+            UserProps up;
+            if (tempUser.getRoleList().get(0).getRole().equals("ROLE_COMPANY")) {
                 Client tempClient = clientRepository.findClientByUserId(a.getPartnerId(tempUser.getId()).getId());
-                UserProps up = new UserProps();
-                up.setFname(tempClient.getFname());
-                up.setLname(tempClient.getLname());
-                up.setEmail(tempClient.getUser().getEmail());
-                convPartners.add(up);
+                up = new UserProps(tempClient.getFname(),tempClient.getLname(),tempClient.getUser().getEmail());               
             } else {
-                Company tempCompany = companyRepository.findCompanyByUserId(a.getPartnerId(tempUser.getId()).getId());
-                UserProps up = new UserProps();
-                up.setFname(tempCompany.getFname());
-                up.setLname(tempCompany.getLname());
-                up.setEmail(tempCompany.getUser().getEmail());
-                up.setCompanyName(tempCompany.getDisplayName());
-                convPartners.add(up);
+                Company comp = companyRepository.findCompanyByUserId(a.getPartnerId(tempUser.getId()).getId());
+                up = new UserProps(comp.getFname(),comp.getLname(), comp.getUser().getEmail(),comp.getDisplayName());               
             }
+            convPartners.add(up);
         }
         return new ResponseEntity<>(convPartners, HttpStatus.OK);
     }
 
-    @GetMapping("/myprops")
+    @GetMapping("/myprops") // send user information to himself
     public ResponseEntity<UserProps> fetchMyData(Principal principal) {
         User tempUser = userRepository.findByEmail(principal.getName()).get();
-        UserProps up = null;
+        UserProps up; 
         if (tempUser.getRoleList().get(0).getRole().equals("ROLE_CLIENT")) {
             Client tempClient = clientRepository.findClientByUserEmail(principal.getName()).get();
-            up = new UserProps();
-            up.setFname(tempClient.getFname());
-            up.setLname(tempClient.getLname());
-            up.setEmail(tempClient.getUser().getEmail());
+            up = new UserProps(tempClient.getFname(),tempClient.getLname(),tempClient.getUser().getEmail());
         } else {
-            Company tempCompany = companyRepository.findCompanyByUserEmail(principal.getName()).get();
-            up = new UserProps();
-            up.setFname(tempCompany.getFname());
-            up.setLname(tempCompany.getLname());
-            up.setEmail(tempCompany.getUser().getEmail());
-            up.setCompanyName(tempCompany.getDisplayName());
+            Company comp = companyRepository.findCompanyByUserEmail(principal.getName()).get();
+            up = new UserProps(comp.getFname(),comp.getLname(),comp.getUser().getEmail(),comp.getDisplayName());
         }
         return new ResponseEntity<>(up, HttpStatus.OK);
     }
 
-    @GetMapping("/load/{userEmail}")
+    @GetMapping("/load/{userEmail}") // loads messages with particular conversation-partner
     public ResponseEntity<List<Message>> fetchMessages(@PathVariable String userEmail, Principal principal) {
         User tempUser = userRepository.findByEmail(principal.getName()).get();
         User otherUser = userRepository.findByEmail(userEmail).get();
@@ -149,19 +105,12 @@ public class ChatController {
         if (tempConv != null) {
             List<Messages> msgs = messagesService.findByConversationId(tempConv.getId()).get();
             for (Messages a : msgs) {
-                Message om = null;
-                if (tempUser.getId() == a.getUserId()) {
-                    om = new Message(principal.getName(), a.getMessage(), a.getTimestamp().toString());
-                } else {
-                    om = new Message(userEmail, a.getMessage(), a.getTimestamp().toString());
-                }
+                String sender = tempUser.getId() == a.getUserId()? principal.getName() : userEmail;
+                Message om = new Message(sender, a.getMessage(), a.getTimestamp().toString());
                 msgsToSend.add(om);
             }
         } else {
-            tempConv = new Conversation();
-            tempConv.setUser1Id(tempUser);
-            tempConv.setUser2Id(otherUser);
-            conversationRepository.save(tempConv);
+            conversationRepository.save(new Conversation(tempUser,otherUser));
         }
         return new ResponseEntity<>(msgsToSend, HttpStatus.OK);
     }
