345efb7bada8c770228ebb43845c75164e83866b
Stefanos Koutsouflakis <koutsouflakis.stefanos@gmail.com>
2021-04-08 13:59:11 +0300

    Panos' register client, captcha

---
+++ src/main/resources/static/js/client/register/register.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+
+    const elements = document.querySelectorAll('[type="password"]');
+    elements.forEach(function (elem) {
+
+        elem.parentNode.querySelector('i').addEventListener('click', function () {
+            if (elem.type === "password") {
+                elem.type = "text";
+                this.className = 'fa fa-eye-slash showpwd';
+            } else {
+                elem.type = "password";
+                this.className = 'fa fa-eye showpwd';
+            }
+        });
+    });
+
+};
+
+function onSubmit(token) {
+    
+    if (document.getElementById("register-form").reportValidity()) {
+        document.getElementById("register-form").submit();
+    }
+   
+}
