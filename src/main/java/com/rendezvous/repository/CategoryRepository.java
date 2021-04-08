873ab25ca774f3d48673f116b0892014b9a77ea1
Stefanos Koutsouflakis <koutsouflakis.stefanos@gmail.com>
2021-04-08 05:46:03 +0300

    search company page, add sorting, i-button

--- src/main/java/com/rendezvous/repository/CategoryRepository.java
+++ src/main/java/com/rendezvous/repository/CategoryRepository.java
@@ -3,6 +3,7 @@ package com.rendezvous.repository;
 import com.rendezvous.entity.CompCategory;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -11,5 +12,7 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface CategoryRepository extends JpaRepository<CompCategory, Integer> {
+   
+    
     List<CompCategory> findAll();
 }
--- src/main/resources/static/js/client/search-company/search-company.js
+++ src/main/resources/static/js/client/search-company/search-company.js
@@ -33,8 +33,8 @@ $(document).ready(function () {
                 var resultsData = [];
                 for (let i = 0; i < searchResults.length; i++) {
                     let card = '<div class="row justify-content-center"><div class="col-11 mycard my-1 rounded bg-white">' +
-                            '<div class="mycard-img"><img src="/img/No_image_available.svg" alt="No-logo-available" /></div>' +
-                            '<div class="mycard-title"><p>' + searchResults[i].displayName + '</p></div>' +
+                            '<div class="mycard-img"><img src="https://eu.ui-avatars.com/api/?name='+searchResults[i].displayName+'" alt="No-logo-available" /></div>' +
+                            '<div class="mycard-title"><p><b>' + searchResults[i].displayName + '</b></p></div>' +
                             '<div class="mycard-telephone"><p class="m-0">Tel: <a href="tel:' + searchResults[i].tel + '">' + searchResults[i].tel + '</a></p></div > ' +
                             '<div class="mycard-address"><p class="m-0">' + searchResults[i].addrStr + ' ' + searchResults[i].addrNo + ', ' + searchResults[i].addrCity + '</p></div>' +
                             '<div class="mycard-link"><a class="btn btn-primary rounded-pill" href="' + full + '/client/date-select?companyId=' + searchResults[i].id + '">Book appointment</a></div>' +
@@ -67,6 +67,14 @@ $(document).ready(function () {
         xhttp.open("GET", full + "/api/v1/client/comp-search?searchTerm=" + searchTerm + "&category=" + document.getElementById("category").value+"&city=" +document.getElementById("city").value , true);
         xhttp.send();
     })
+
+  $("#searchInfo").popover({
+        content: "Your search can be based on the company name or the telephone number",
+        trigger: 'hover'
+    });
+
+   
 });
+
 
 
